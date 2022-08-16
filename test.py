f914bb50f287817668cad7ca64201806a3e52e49
LizardWiz <lizardwiz@hotmail.com>
2022-08-16 16:57:09 -0700

    t

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 import subprocess
 
-def runcmd(cmd, verbose = False, *args, **kwargs):
-
-    process = subprocess.Popen(
-        cmd,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE,
-        text = True,
-        shell = True
-    )
-    std_out, std_err = process.communicate()
-    if verbose:
-        print(std_out.strip(), std_err)
-    pass
+#def runcmd(cmd, verbose = False, *args, **kwargs):
+#
+#    process = subprocess.Popen(
+#        cmd,
+#        stdout = subprocess.PIPE,
+##        stderr = subprocess.PIPE,
+ #       text = True,
+ #       shell = True
+ #   )
+ #   std_out, std_err = process.communicate()
+ #   if verbose:
+ #       print(std_out.strip(), std_err)
+ #   pass
 
 print("jello world")
 print("jelly world")
-runcmd('wget "https://mega.nz/folder/DfBWGTjA#BFcNX-XcMEnY-cdFDWTx1Q/file/WO43iKLZ"')
+
+child = subprocess.Popen(['wget','https://mega.nz/folder/DfBWGTjA#BFcNX-XcMEnY-cdFDWTx1Q/file/WO43iKLZ'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+print(child.stdout.read())
+#runcmd('wget "https://mega.nz/folder/DfBWGTjA#BFcNX-XcMEnY-cdFDWTx1Q/file/WO43iKLZ"')
