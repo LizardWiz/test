09c73e8fc21dc0f5824fab880a96d7e7ed682584
LizardWiz <lizardwiz@hotmail.com>
2022-08-16 16:47:44 -0700

    Real new commit

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 print("jello world")
 print("jelly world")
+import subprocess
+
+def runcmd(cmd, verbose = False, *args, **kwargs):
+
+    process = subprocess.Popen(
+        cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text = True,
+        shell = True
+    )
+    std_out, std_err = process.communicate()
+    if verbose:
+        print(std_out.strip(), std_err)
+    pass
+
 runcmd("wget \"https://mega.nz/folder/DfBWGTjA#BFcNX-XcMEnY-cdFDWTx1Q/file/WO43iKLZ\"")
